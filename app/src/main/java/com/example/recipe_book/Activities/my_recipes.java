9cad788ef4c2c65b32dabdfca9c1efe18b645e66
Michał <michal.pasierbiewicz.314@gmail.com>
2021-03-31 18:10:18 +0200

    Testy listy przepisów

---
+++ app/src/main/java/com/example/recipe_book/Activities/my_recipes.java
@@ -0,0 +1,14 @@
+package com.example.recipe_book.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.recipe_book.R;
+
+public class my_recipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_recipes);
+    }
+}
--- app/src/main/java/com/example/recipe_book/MainActivity.java
+++ app/src/main/java/com/example/recipe_book/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.recipe_book;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.recipe_book.Activities.my_recipes;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button recipe_book;
+    private Button add_recipe;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recipe_book = findViewById(R.id.my_recippes_button);
+        recipe_book.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                open_recipes_viev();
+            }
+        });
     }
+
+    private void open_recipes_viev()
+    {
+        Intent open = new Intent(this, my_recipes.class);
+        startActivity(open);
+    }
+
 }
